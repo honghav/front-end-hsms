478981ab4f79b95839f203b478505adc46e54e8b
honghav <158126590+honghav@users.noreply.github.com>
2025-02-23 19:57:46 +0700

    database paht

--- database/hight_schoolii.sql
+++ database/hight_schoolii.sql
@@ -25,9 +25,10 @@ create table teachers(
     tea_gender VARCHAR(50) not null,
     tea_subject INT not null,
     tea_username VARCHAR(255) not null,
+    tea_password VARCHAR(255) not null,
     tea_dob date not null,
-    tea_ph_number VARCHAR(255),
-    tea_profile VARCHAR(255),
+    tea_ph_number VARCHAR(255) null,
+    tea_profile VARCHAR(255) null,
     FOREIGN KEY (tea_subject) REFERENCES subjects(sub_id) ON DELETE SET NULL
 
 );
@@ -35,12 +36,15 @@ create table teachers(
 create table admin(
     adm_id int primary key,
     adm_username varchar(255),
-    adm_profile varchar(255)
+    adm_profile varchar(255),
+    adm_password varchar(255)
 );
 
 create table subjects(
     sub_id int primary key,
     sub_name VARCHAR(255) not null,
+    sub_image VARCHAR(255) null
+
 );
 create table courses (
     cou_id int primary key,
@@ -54,9 +58,9 @@ create table attendances(
     att_id int primary key,
     att_student_id int not null,
     att_code varchar(255),
-    att_startime date not null,
-    att_subtime date not null,
-    att_endtime date not null,
+    att_startime datetime not null,
+    att_subtime datetime not null,
+    att_endtime datetime not null,
     att_course_id int not null,
     FOREIGN KEY (att_student_id) REFERENCES students(stu_id) ON DELETE CASCADE,
     FOREIGN KEY (att_course_id) REFERENCES courses(cou_id) ON DELETE CASCADE
@@ -75,8 +79,9 @@ create table scores (
 
 create table schedule(
     sch_id int primary key,
-    sch_start_time date not null,
-    sch_end_time date not null,
+    sch_start_time time not null,
+    sch_end_time time not null,
+    sch_day enum('monday','tuesday','wednesday','thursday','friday','saturday','sunday') not null,
     sch_cou_id int not null,
     FOREIGN KEY (sch_cou_id) REFERENCES courses(cou_id) ON DELETE CASCADE
 );
@@ -86,10 +91,59 @@ create table schedule(
 create table documents (
     doc_id int primary key,
     doc_type enum('lecture','homework','none')default'none',
+    doc_name varchar(255) not null,
+    doc_deatial varchar(255) not null,
     doc_cou_id int not null,
+    doc_date datetime not null,
     FOREIGN KEY (doc_cou_id) REFERENCES courses(cou_id) ON DELETE CASCADE
 );
 
+-- recovery table 
+
+create table re_student(
+
+    re_stu_id int PRIMARY Key,
+    re_stu_fname varchar(255) not null,
+    re_stu_grade int not null,
+    re_stu_username varchar(255) not null,
+    re_stu_password VARCHAR(255) not null,
+    re_stu_group VARCHAR(255) not null,
+    re_stu_gender VARCHAR(255) not null,
+    re_stu_dob DATE not null,
+    re_stu_ph_number int not null,
+    re_stu_parent_number int not null,
+    re_stu_profile VARCHAR(255),
+    re_admin_id int not null,
+    FOREIGN KEY (re_admin_id) REFERENCES admin(adm_id) ON DELETE CASCADE 
+);
+create table re_course(
+    re_cou_id int primary key,
+    re_cou_sub_id int not null,
+    re_cou_tea_id int not null,
+    FOREIGN KEY (re_cou_sub_id) REFERENCES subjects(sub_id) ON DELETE CASCADE,
+    FOREIGN KEY (re_cou_tea_id) REFERENCES teachers(tea_id) ON DELETE CASCADE
+);
+create table re_attendances(
+    re_att_id int primary key,
+    re_att_student_id int not null,
+    re_att_code varchar(255),
+    re_att_startime datetime not null,
+    re_att_subtime datetime not null,
+    re_att_endtime datetime not null,
+    re_att_course_id int not null,
+    FOREIGN KEY (re_att_student_id) REFERENCES students(stu_id) ON DELETE CASCADE,
+    FOREIGN KEY (re_att_course_id) REFERENCES courses(cou_id) ON DELETE CASCADE
+
+);
+create table re_scores (
+    re_sco_id int primary key,
+    re_sco_point int not null,
+    re_sco_month date not null,
+    re_sco_cou_id int not null,
+    re_sco_stu_id int not null,
+    FOREIGN KEY (re_sco_cou_id) REFERENCES courses(cou_id) ON DELETE CASCADE,
+    FOREIGN KEY (re_sco_stu_id) REFERENCES students(stu_id) ON DELETE CASCADE
+);
 
 
 
